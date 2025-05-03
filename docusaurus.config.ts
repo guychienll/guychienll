d36a0987fcac5da920d18ffa84c86d750c20dd2b
guychienll <guychienll@gmail.com>
2025-05-04 00:20:43 +0800

    feat: replace png with webp and resize avatar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,6 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -101,7 +101,7 @@ const config: Config = {
       title: "guychienll",
       logo: {
         alt: "logo",
-        src: "img/notes.png",
+        src: "img/notes.webp",
       },
       items: [
         {
